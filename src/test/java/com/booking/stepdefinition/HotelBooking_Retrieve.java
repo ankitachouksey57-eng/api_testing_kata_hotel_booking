83dd473f0ccc14cc6024a61d400258d7deb635db
Ankita Chouksey <ankitachouksey57@gmail.com>
2025-08-20 22:23:01 +0200

    feature and stepdef file added for retrieval operation with success and negative scenarios

---
+++ src/test/java/com/booking/stepdefinition/HotelBooking_Retrieve.java
@@ -0,0 +1,119 @@
+package com.booking.stepdefinition;
+
+import com.booking.util.BookingContext;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.empty;
+
+@RequiredArgsConstructor
+public class HotelBooking_Retrieve {
+
+    private final BookingContext context;
+    Response response;
+    @When("the user retrieves booking with booking ID")
+    public void theUserRetrievesBookingWithBookingID() {
+
+        //        As response for create booking is empty so we will get the
+        //        booking id using get booking details by roomID
+        String tokenValue = context.getToken();
+        final int roomId = Integer.parseInt(BookingContext.getRoomIds().getLast().toString());
+        response = context.requestSetup().cookie("token", tokenValue).when()
+                .get(context.session.get("endpoint").toString() + "?roomid=" + roomId);
+
+        //        saving the value of bookingID
+        BookingContext.addBookingId(response.jsonPath().get("bookings[0].bookingid"));
+
+
+        final Integer storedBookingId = BookingContext.getBookingIds().getLast();
+        if (storedBookingId != null) {
+            context.response = context.requestSetup().cookie("token", tokenValue).when()
+                    .get(context.session.get("endpoint").toString() +"/"+ storedBookingId);
+
+            System.out.println("The user retrieves booking with booking ID: " + response.body());
+        } else {
+            throw new RuntimeException("Booking ID not available for GET request.");
+        }
+    }
+
+    @Then("the response body should contain the response with JSON schema {string}")
+    public void theResponseBodyShouldContainBookingDetailsWithID(final String schemaFileName) {
+        context.response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/" + schemaFileName));
+    }
+
+    @Then("user should get the response code {int}")
+    public void userShouldGetTheResponseCode(final Integer statusCode) {
+        assertThat(Long.valueOf(statusCode)).isEqualTo(Long.valueOf(context.response.getStatusCode()));
+    }
+
+    @And("the response body should contain the correct values")
+    public void theResponseBodyShouldContainTheCorrectValues() {
+        System.out.println("response body >>"+response.body().asString());
+        String name = response.jsonPath().get("bookings[0].firstname").toString();
+        assertThat(name).isEqualTo("Ankita");
+        String lastname = response.jsonPath().get("bookings[0].lastname").toString();
+        assertThat(lastname).isEqualTo("Chouksey");
+    }
+
+    @When("the user retrieves booking with invalid booking ID {int}")
+    public void theUserRetrievesBookingWithInvalidBookingID(final Integer bookingid) {
+        String tokenValue = context.getToken();
+        context.response = context.requestSetup().cookie("token", tokenValue).when()
+                .get(context.session.get("endpoint").toString() + bookingid);
+    }
+
+    @And("the response body should contain an empty bookings array")
+    public void theResponseBodyShouldContainEmptyBookings() {
+        context.response.then().assertThat().body("bookings", empty());
+    }
+
+    @When("the user retrieves booking with room ID")
+    public void theUserRetrievesBookingWithRoomID() {
+        final Integer roomId = BookingContext.getRoomIds().getLast();
+        String tokenValue = context.getToken();
+        response = context.requestSetup().cookie("token", tokenValue).when()
+                .get(context.session.get("endpoint").toString() + "?roomid=" + roomId);
+        BookingContext.addBookingId(response.jsonPath().get("bookings[0].bookingid"));
+    }
+
+    @When("the user retrieves booking with invalid room ID {int}")
+    public void theUserRetrievesBookingWithInvalidRoomID(final Integer roomid) {
+        String tokenValue = context.getToken();
+        context.response = context.requestSetup().cookie("token", tokenValue).when()
+                .get(context.session.get("endpoint").toString() + "?roomid=" + roomid);
+    }
+
+    @And("the response body should contain the response with correct values")
+    public void theResponseBodyShouldContainTheResponseWithCorrectValues() {
+        String name = response.jsonPath().get("bookings[0].firstname").toString();
+        assertThat(name).isEqualTo("Aarya");
+        String lastname = response.jsonPath().get("bookings[7].lastname").toString();
+        assertThat(lastname).isEqualTo("Chouksey");
+    }
+
+    @And("the response body should contain the response with the correct values")
+    public void theResponseBodyShouldContainTheResponseWithTheCorrectValues() {
+        String name = response.jsonPath().get("bookings[0].firstname").toString();
+        assertThat(name).isEqualTo("Ankita");
+        String lastname = response.jsonPath().get("bookings[7].lastname").toString();
+        assertThat(lastname).isEqualTo("Chouksey");
+    }
+
+    @And("validate the response with updated values")
+    public void validateTheResponseWithUpdatedValues() {
+        final Integer roomId = BookingContext.getRoomIds().getLast();
+        String tokenValue = context.getToken();
+        response = context.requestSetup().cookie("token", tokenValue).when()
+                .get(context.session.get("endpoint").toString() + "?roomid=" + roomId);
+        System.out.println("response for retrieval with room ID >> "+response.asString());
+        String name = response.jsonPath().get("bookings[0].firstname").toString();
+        assertThat(name).isEqualTo("Aarya");
+        String lastname = response.jsonPath().get("bookings[7].lastname").toString();
+        assertThat(lastname).isEqualTo("Chouksey");
+    }
+}
---
+++ src/test/resources/features/hotelBooking_retrieve.feature
@@ -0,0 +1,53 @@
+@bookingAPI @getBookingByID
+Feature: Booking Retrieval API Tests
+
+  Background:
+    Given I have a valid token
+
+  @getBookingByRoomID
+  Scenario Outline: Retrieve bookings with valid Room IDs
+    Given user has access to endpoint "/booking"
+    When the user books a room with the following booking details
+      | firstname   | lastname   | email   | phone   | checkin   | checkout   |
+      | <firstname> | <lastname> | <email> | <phone> | <checkin> | <checkout> |
+    Then the response status code should be 200
+    When the user retrieves booking with room ID
+    Then user should get the response code 200
+    And the response body should contain the correct values
+    Examples:
+      | firstname | lastname | email              | phone         | checkin    | checkout   |
+      | Ankita    | Chouksey | ankichou@gmail.com | 475871872462  | 2025-09-12 | 2025-09-18 |
+
+
+    #  get booking details by ID is failing
+  @getBookingByBookingID
+  Scenario: Retrieve bookings with valid Booking IDs
+    Given user has access to endpoint "/booking"
+    When the user books a room with the following booking details
+      | firstname | lastname | email                | phone         | checkin    | checkout   |
+      | Ankita    | Chouksey | Ankitachou@gmail.com | 4657525384457 | 2025-09-22 | 2025-09-25 |
+    Then the response status code should be 200
+    When the user retrieves booking with booking ID
+    Then user should get the response code 200
+    And the response body should contain the response with correct values
+
+  @getBookingByInvalidBookingID
+  Scenario: Retrieve bookings with invalid Booking IDs
+    Given user has access to endpoint "/booking"
+    When the user books a room with the following booking details
+      | firstname | lastname | email                | phone         | checkin    | checkout   |
+      | Ankita    | Chouksey | Ankitachou@gmail.com | 4657525384457 | 2025-09-22 | 2025-09-25 |
+    Then the response status code should be 200
+    When the user retrieves booking with invalid booking ID 0
+    Then user should get the response code 404
+
+  @getBookingByInvalidRoomID
+  Scenario: Retrieve bookings with invalid room IDs
+    Given user has access to endpoint "/booking"
+    When the user books a room with the following booking details
+      | firstname | lastname | email                | phone         | checkin    | checkout   |
+      | Ankita    | Chouksey | Ankitachou@gmail.com | 4657525384457 | 2025-09-22 | 2025-09-25 |
+    Then the response status code should be 200
+    When the user retrieves booking with invalid room ID 0
+    Then user should get the response code 200
+    And the response body should contain an empty bookings array

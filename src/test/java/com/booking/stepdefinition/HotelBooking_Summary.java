47259d7a5c6589d241bc0b0015ef9c38489570e2
Ankita Chouksey <ankitachouksey57@gmail.com>
2025-08-20 23:01:57 +0200

    booking summary feature and stepdef scenarios added

---
+++ src/test/java/com/booking/stepdefinition/HotelBooking_Summary.java
@@ -0,0 +1,43 @@
+package com.booking.stepdefinition;
+
+import com.booking.util.BookingContext;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RequiredArgsConstructor
+public class HotelBooking_Summary {
+
+    private Response response;
+    private final BookingContext context;
+
+    @When("the user retrieves booking summary {string} with roomid")
+    public void theUserRetrievesBookingSummaryWithRoomId(final String endpoint) {
+        context.session.put("endpoint", endpoint);
+        final Integer roomId = BookingContext.getRoomIds().getLast();
+        response = context.requestSetup().when().get(context.session.get("endpoint").toString() + "?roomid=" + roomId);
+    System.out.println("booking summary response>> "+response.body().asString());
+    }
+
+    @Then("the response should contain the booking dates {string} and {string} for room id and status code {int}")
+    public void theResponseShouldContainTheBookingDatesForRoom(final String expectedCheckInDate, final String expectedCheckOutDate, final int expectedStatusCode) {
+
+        final String checkIn = response.jsonPath().getString("bookings[0].bookingDates.checkin");
+        final String checkOut = response.jsonPath().getString("bookings[0].bookingDates.checkout");
+
+        assertThat(checkIn).isNotNull();
+        assertThat(checkOut).isNotNull();
+        assertThat(expectedCheckInDate).isEqualTo(checkIn);
+        assertThat(expectedCheckOutDate).isEqualTo(checkOut);
+        assertThat(expectedStatusCode).isEqualTo(response.getStatusCode());
+    }
+
+    @When("the user retrieves booking summary {string} with invalid roomid {int}")
+    public void theUserRetrievesBookingSummaryWithInvalidRoomId(final String endpoint, final int roomid) {
+        context.session.put("endpoint", endpoint);
+        context.response = context.requestSetup().when().get(context.session.get("endpoint").toString() + "?roomid=" + roomid);
+    }
+}
---
+++ src/test/resources/features/hotelBooking_summary.feature
@@ -0,0 +1,26 @@
+@bookingAPI @bookingSummary @noAuth
+Feature: Booking Summary API Tests
+
+  Background:
+    Given I have a valid token
+
+#These scenario's are failing as it seems summary API is sending null response
+  @getBookingSummaryByValidID
+  Scenario Outline: Verify booking summary for different room ids
+    Given user has access to endpoint "/booking"
+    When the user books a room with the following booking details
+      | firstname   | lastname   | email   | phone   | checkin   | checkout   |
+      | <firstname> | <lastname> | <email> | <phone> | <checkin> | <checkout> |
+    Then the response status code should be 200
+    When the user retrieves booking summary "/booking/summary" with roomid
+    Then the response should contain the booking dates "<checkin>" and "<checkout>" for room id and status code 200
+    Examples:
+      | firstname | lastname | email                | phone        | checkin    | checkout   |
+      | Ankita    | Chouksey | ankichou@gmail.com   | 475871872462 | 2025-09-12 | 2025-09-18 |
+
+  @getBookingSummaryByInvalidID
+  Scenario: Verify booking summary for invalid room id
+    Given user has access to endpoint "/booking"
+    When the user retrieves booking summary "/booking/summary" with invalid roomid 0
+    Then user should get the response code 200
+    And the response body should contain an empty bookings array
